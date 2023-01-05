98c55efa08d20867025d092e143769bd6749f07f
AniltonJunior <viganigojr@gmail.com>
2023-01-04 23:54:55 -0300

    Order, Instant, ISO 8601 - pt 02

---
+++ src/main/java/com/springboot/course/resources/OrderResource.java
@@ -0,0 +1,33 @@
+package com.springboot.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.course.entities.Order;
+import com.springboot.course.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll(){
+		List<Order> Order = orderService.findAll();
+		return ResponseEntity.ok().body(Order); 
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id){
+		Order Order = orderService.findById(id);
+		return ResponseEntity.ok().body(Order);
+	}
+}
